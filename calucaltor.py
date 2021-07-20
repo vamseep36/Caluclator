d7c07c9c3b270a12a33ea568f245416fc11ed879
Vishnu Vamsee Paningipalli <62656098+vamseep36@users.noreply.github.com>
2021-07-20 20:37:38 +0530

    Create Update.py

--- calucaltor.py
+++ calucaltor.py
@@ -61,31 +61,23 @@ def clear():
 if __name__ == "__main__": 
     # create a GUI window 
     gui = Tk() 
-  
     # set the background colour of GUI window 
     gui.configure(background="light green") 
-  
     # set the title of GUI window 
     gui.title("Simple Calculator") 
-  
     # set the configuration of GUI window 
     gui.geometry("270x150") 
-  
     # StringVar() is the variable class 
     # we create an instance of this class 
     equation = StringVar() 
-  
     # create the text entry box for 
     # showing the expression . 
     expression_field = Entry(gui, textvariable=equation) 
-  
     # grid method is used for placing 
     # the widgets at respective positions 
     # in table like structure . 
     expression_field.grid(columnspan=4, ipadx=70) 
-  
     equation.set('enter your expression') 
-  
     # create a Buttons and place at a particular 
     # location inside the root window . 
     # when user press the button, the command or 
